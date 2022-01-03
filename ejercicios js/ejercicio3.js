05f9fbf040151c4dc07f6e25ffbff4f8132ad793
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-01-03 16:03:17 +0100

    Update ejercicio3
    Realización del ejercicio 3

--- ejercicios js/ejercicio3.js
+++ ejercicios js/ejercicio3.js
@@ -2,3 +2,6 @@
 Crear una función que devuelva un array con el numero de elementos indicado,
 inicializados al valor suministrado.
 */
+var arr = [37,356,12,738,432];
+var len= arr.length;
+console.log(arr.length);
