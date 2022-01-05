2aaf01f5f844ec8fe66bd6ead4f083e2ea7ebfba
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-01-05 12:09:10 +0100

    Update calculator
    Actualización calculadora

---
+++ ejercicios DOM/calculadora/javascript.js
@@ -0,0 +1,14 @@
+
+var listenerBtn = [];
+
+    listenerBtn.push(document.getElementById("btnNum0"));
+    listenerBtn.push(document.getElementById("btnNum1"));
+    listenerBtn.push(document.getElementById("btnNum2"));
+    listenerBtn.push(document.getElementById("btnNum3"));
+    listenerBtn.push(document.getElementById("btnNum4"));
+    listenerBtn.push(document.getElementById("btnNum5"));
+    listenerBtn.push(document.getElementById("btnNum6"));
+    listenerBtn.push(document.getElementById("btnNum7"));
+    listenerBtn.push(document.getElementById("btnNum8"));
+    listenerBtn.push(document.getElementById("btnNum9"));
+
