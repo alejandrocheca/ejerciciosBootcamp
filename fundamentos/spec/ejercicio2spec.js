e6b2663d5a76675e8840688abf64fd144bcac2ac
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-01-13 10:14:15 +0100

    Update ejercicio2 Jasmine
    Actualizacion ejercicio2 con JAsmine

--- fundamentos/spec/ejercicio2spec.js
+++ fundamentos/spec/ejercicio2spec.js
@@ -1,20 +1,6 @@
 describe(`ejercicio2`, () => {
-    it('Menor Mayor Igual.', () => {
-        spyOn(Math, 'random').and.returnValue(0.50);
-        const promptSpy = spyOn(window, 'prompt').and.returnValues('30');
-        const alertSpy = spyOn(window, 'alert').and.stub();
-        numAleatorio();
-        expect(promptSpy.calls.count()).toBe(3);
-        expect(alertSpy.calls.count()).toBe(3);
-        expect(promptSpy.calls.all()[0].args[0]).toBe('Introduce un número:');
-        expect(alertSpy.calls.all()[0].args[0]).toBe('El número es mayor que el introducido. Inténtalo otra vez.');
-        expect(promptSpy.calls.all()[1].args[0]).toBe('Introduce un número:');
-        expect(alertSpy.calls.all()[1].args[0]).toBe('El número es menor que el introducido. Inténtalo otra vez.');
-        expect(promptSpy.calls.all()[2].args[0]).toBe('Introduce un número:');
-        expect(alertSpy.calls.all()[2].args[0]).toBe('Numero correcto.');
-    });
     it('Comprobar numero.', () => {
-        const promptSpy = spyOn(window, 'prompt').and.returnValues('3', '1', '2' );
+        const promptSpy = spyOn(window, 'prompt').and.returnValues('1', '2', '3' );
         const alertSpy = spyOn(window, 'alert').and.stub();
         spyOn(Math, 'random').and.returnValue(0.02);
         numAleatorio();
@@ -24,6 +10,24 @@ describe(`ejercicio2`, () => {
         expect(alertSpy.calls.all()[1].args[0]).toBe('El número es mayor que el introducido. Inténtalo otra vez.');
         expect(alertSpy.calls.all()[2].args[0]).toBe('Enhorabuena. Has acertado.');
     });
+    it('Igualar numero', () => {
+        spyOn(Math, 'random').and.returnValue(0.50);
+        const promptSpy = spyOn(window, 'prompt').and.returnValues('30');
+        const alertSpy = spyOn(window, 'alert').and.stub();
+        numAleatorio();
+        expect(promptSpy.calls.count()).toBe(3);
+        expect(alertSpy.calls.count()).toBe(3);
+
+        expect(promptSpy.calls.all()[0].args[0]).toBe('Introduce un número:');
+        expect(alertSpy.calls.all()[0].args[0]).toBe('El número es mayor que el introducido. Inténtalo otra vez.');
+
+        expect(promptSpy.calls.all()[1].args[0]).toBe('Introduce un número:');
+        expect(alertSpy.calls.all()[1].args[0]).toBe('El número es menor que el introducido. Inténtalo otra vez.');
+
+        expect(promptSpy.calls.all()[2].args[0]).toBe('Introduce un número:');
+        expect(alertSpy.calls.all()[2].args[0]).toBe('Numero correcto.');
+    });
+    
     it('Fallo', () => {
         const promptSpy = spyOn(window, 'prompt').and.returnValues(0)
         const alertSpy = spyOn(window, 'alert').and.stub()
@@ -39,26 +43,12 @@ describe(`ejercicio2`, () => {
     describe(`ejercicio2 ${caso.name}`, () => {
         let adivinaAleatorio = null;
         const respuesta = 10;
-        const numAcertar = 100;
+        const numAcertar = Math.random();
         beforeAll(() => {
             spyOn(Math, 'random').and.returnValue(0.945569);
         });
         beforeEach(() => {
             numeroAleatorio = new caso(respuesta, numAcertar);
-        });
-        it('Mi número es mayor.', () => {
-            numeroAleatorio.comprNum(82)
-
-            expect(numeroAleatorio.msj).toBe('Mi número es mayor.');
-            expect(numeroAleatorio.intentos).toBe(1);
-            expect(numeroAleatorio.numAcertado).toBeFalse();
-        });
-        it('Mi número es menor.', () => {
-            numeroAleatorio.comprNum(84);
-            numeroAleatorio.comprNum(84);
-            expect(numeroAleatorio.msj).toBe('Mi número es menor.');
-            expect(numeroAleatorio.intentos).toBe(2);
-            expect(numeroAleatorio.numAcertado).toBeFalse();
         });
         it('Intentos', () => {
             for (let num = 1; num < respuesta; num++) {
@@ -70,13 +60,27 @@ describe(`ejercicio2`, () => {
             expect(numeroAleatorio.intentos).toBe(respuesta);
             expect(numeroAleatorio.numAcertado).toBeFalse();
         });
+        it('El número es mayor.', () => {
+            numeroAleatorio.comprNum(respuesta)
+            expect(numeroAleatorio.msj).toBe('El número es mayor que el introducido. Inténtalo otra vez.');
+            expect(numeroAleatorio.intentos).toBe(1);
+            expect(numeroAleatorio.numAcertado).toBeFalse();
+        });
+        it('El número es menor.', () => {
+            numeroAleatorio.comprNum(respuesta);
+            numeroAleatorio.comprNum(respuesta);
+            expect(numeroAleatorio.msj).toBe('El número es menor que el introducido. Inténtalo otra vez.');
+            expect(numeroAleatorio.intentos).toBe(2);
+            expect(numeroAleatorio.numAcertado).toBeFalse();
+        });
+       
         it('Enhorabuena. Has acertado.', () => {
             numeroAleatorio.comprNum(50);
             expect(numeroAleatorio.msj).toBe('Enhorabuena. Has acertado.');
             expect(numeroAleatorio.numAcertado).toBeTrue();
         });
         it('Juego terminado', () => {
-            for (let i = 0; i < respuesta; i++) {
+            for (let num = 0; num < respuesta; num++) {
                 numeroAleatorio.comprNum(3);
             }
             expect(() => numeroAleatorio.comprNum(3)).toThrow();
