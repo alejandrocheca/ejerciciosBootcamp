0876c1b9b8d677f41de813a720dcfd54da458820
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-01-14 07:51:23 +0100

    Update calculator
    Realización ejercicio calculadora co Jasmine

---
+++ fundamentos/spec/calculadoraspec.js
@@ -0,0 +1,26 @@
+describe('Calculadora', () => {
+   
+    const num1Operado = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const num2Operado = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const operacion = ['+','-','x','/','='];
+    let resultado;
+    expect(Calculadora.boton).toBe(2);
+    it('Validar boton numero.', () => {
+
+        expect(num1Operado.boton).toBe(num1Operado);
+        expect(num2Operado.boton).toBe(num2Operado);
+        
+    });
+    it('Validar boton operacion.', () => {
+
+        expect(operacion.boton).toBe(operacion);
+        
+    });
+    it('Operar.', () => {
+
+        if(num1Operado.boton =!null && num2Operado.boton =!null && operacion.boton =!null){
+            resultado = num1Operado.boton + operacion.boton + num2Operado.boton;
+            console.log(resultado);
+        }
+    }); 
+});
